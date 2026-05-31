dbe23b1c434e4b903f55870c84ac4a65549cbcfe
attackuwu <142084242+attackuwu@users.noreply.github.com>
2026-05-31 22:07:26 +0500

    Update udev_install.h

--- src/udev_install.h
+++ src/udev_install.h
@@ -1,11 +1,5 @@
 #pragma once
 #include <glib.h>
 
-/* TRUE if our udev rule file is already installed. */
 gboolean udev_rules_installed (void);
-
-/*
- * Install udev rules via pkexec (Polkit password dialog).
- * Returns TRUE on success.
- */
 gboolean udev_install_rules (GError **error);
